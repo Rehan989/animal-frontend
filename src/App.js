57c0ba9d933864efc05ca579995adef42f6f8866
Robin8766 <admin@prodev.pro>
2022-01-27 11:24:01 +0530

    UI template for signin completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center">
+      <div className='container'>
+        <div className='position-absolute top-50 start-50 translate-middle'>
+          <h1 className='mb-5'>Welcome</h1>
+          <form>
+            <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+              <option selected>Who are you?</option>
+              <option value="1">Doctor</option>
+              <option value="2">Technician</option>
+            </select>
+            <div class="form-floating mb-3">
+              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+              <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating">
+              <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+              <label for="floatingPassword">Password</label>
+            </div>
+            <button type="submit" class="btn mt-3 btn-primary">Login</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
