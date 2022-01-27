96aed4159bdc23201458d92062a9c9695ba95fc5
Robin8766 <admin@prodev.pro>
2022-01-27 11:56:13 +0530

    Migrated signin file to a component and Menu created

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signin from './Components/Signin';
+import Farmer from './Components/Registration/Farmer';
 
 function App() {
   return (
-    <div className="text-center">
-      <div className='container'>
-        <div className='position-absolute top-50 start-50 translate-middle'>
-          <h1 className='mb-5'>Welcome</h1>
-          <form>
-            <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-              <option selected>Who are you?</option>
-              <option value="1">Doctor</option>
-              <option value="2">Technician</option>
-            </select>
-            <div class="form-floating mb-3">
-              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
-              <label for="floatingInput">Email address</label>
-            </div>
-            <div class="form-floating">
-              <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
-              <label for="floatingPassword">Password</label>
-            </div>
-            <button type="submit" class="btn mt-3 btn-primary">Login</button>
-          </form>
-        </div>
-      </div>
-    </div>
+    <>
+      <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Signin />} />
+            <Route exact path="/register/farmer" element={<Farmer />} />
+          </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar = (props) => {
+    return <div>
+        <nav className="nav mt-3 mx-5 nav-pills nav-fill">
+            <a className="nav-link" aria-current="page" href="/search/farmer">Search Farmer</a>
+            <a className="nav-link" href="/register/farmer">Register Farmer</a>
+            <a className="nav-link" href="/register/animal">Register Animal</a>
+            <a className="nav-link" href="/register/pd">Register PD</a>
+            <a className="nav-link" href="/register/bullsemen">Register BullSemen</a>
+            <a className="nav-link" href="/aidetails">AI Details</a>
+            <a className="nav-link" href="/calfdetails">Calf Details</a>
+            <a className="nav-link" href="/report">Report</a>
+            <a className="nav-link" href="/summaries">Summaries</a>
+        </nav>
+    </div>;
+};
+
+export default Navbar;
