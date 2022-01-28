276f97f9cc766c80ff6e9fd97578df2055d0f89b
Robin8766 <admin@prodev.pro>
2022-01-28 11:22:11 +0530

    Search farmer ui completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signin from './Components/Signin';
 import Farmer from './Components/Registration/Farmer';
+import SearchFarmer from './Components/Search/SearchFarmer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Signin />} />
             <Route exact path="/register/farmer" element={<Farmer />} />
+            <Route exact path="/search/farmer" element={<SearchFarmer />} />
           </Routes>
       </BrowserRouter>
     </>
---
+++ src/Components/Search/SearchFarmer.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Navbar from '../Navbar';
+
+const SearchFarmer = () => {
+  return <div>
+    <Navbar />
+    <div className='container mt-5'>
+      <h1>
+        Search Farmer
+      </h1>
+
+      <form>
+        <div className="input-group mb-3">
+          <span className="input-group-text" id="basic-addon1">Enter name of the farmer</span>
+          <input type="text" className="form-control" placeholder="name" aria-label="Username" aria-describedby="basic-addon1" />
+          <button type="submit" className="btn btn-primary">Search</button>
+        </div>
+      </form>
+
+      <div>
+        <div className='my-5'>
+          <div class="card">
+            <ul class="list-group list-group-flush">
+              <button className='btn' type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"><li class="list-group-item">name  - mobile number</li></button>
+              <div class="collapse" id="collapseExample">
+                <div class="card card-body">
+                  Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+                </div>
+              </div>
+            </ul>
+          </div>
+        </div>
+
+        <nav aria-label="Page navigation example">
+          <ul className="pagination">
+            <li className="page-item"><button className="page-link" >Previous</button></li>
+            <li className="page-item"><button className="page-link" >1</button></li>
+            <li className="page-item"><button className="page-link" >2</button></li>
+            <li className="page-item"><button className="page-link" >3</button></li>
+            <li className="page-item"><button className="page-link" >Next</button></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </div>;
+};
+
+export default SearchFarmer;
