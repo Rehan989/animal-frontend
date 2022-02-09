1fd60932f86c86b6ed67f4854c64f4a87c610f97
Robin8766 <admin@prodev.pro>
2022-02-09 14:19:24 +0530

    Animal registration completed

--- src/Components/Registration/Animal.js
+++ src/Components/Registration/Animal.js
@@ -1,39 +1,196 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../Navbar';
 
-const Animal = () => {
+const Animal = (props) => {
+    const [SpeciesLoading, setSpeciesLoading] = useState(true);
+    const [showFarmer, setshowFarmer] = useState(false);
+    const [farmers, setFarmers] = useState([]);
+    const [Species, setSpecies] = useState([]);
+    const [creds, setCreds] = useState({
+        tagNumber: "",
+        date: "",
+        species: "",
+        breed: "",
+        age: "",
+        noOfCalvings: "",
+        farmerId: "",
+        farmerName: ""
+    });
+
+    const onChange = (e) => {
+        setCreds({ ...creds, [e.target.name]: e.target.value });
+    }
+
+    const fetchAnimalSpecies = async () => {
+        const response = await fetch(`${process.env.REACT_APP_BACKEND_HOST}/api/get/species/`, {
+            "method": "GET",
+            headers: {
+                "Content-Type": "application/json",
+                'Accept': 'application/json'
+            }
+        })
+        const data = await response.json()
+        setSpecies(data);
+        setSpeciesLoading(false);
+    }
+
+    const searchFarmer = async (e) => {
+        try {
+            if (creds.farmerName.length <= 3) {
+                props.setshowAlert("Error", "Search query must be greater than 3 carachters")
+                return
+            }
+            const response = await fetch(`${process.env.REACT_APP_BACKEND_HOST}/api/search/farmer/${creds.farmerName}/`, {
+                "method": "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    'Accept': 'application/json',
+                    'auth-token': `${localStorage.getItem('auth_token')}`
+                },
+            })
+            const data = await response.json()
+            console.log(data);
+            if (data.success) {
+                setFarmers(data.farmers)
+                if (data.farmers.length === 0) {
+                    props.setshowAlert("Error", `Doctor with the name not found!`)
+                    setshowFarmer(false)
+                    return
+                }
+                setshowFarmer(true)
+            }
+            else {
+                props.setshowAlert("Error", `${data.error}`)
+                setshowFarmer(false)
+                return
+            }
+        }
+        catch (error) {
+            console.log(error);
+            setshowFarmer(false)
+            props.setshowAlert("Error", "Internal Server Error")
+        }
+    }
+
+    const handleAnimalRegister = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await fetch(`${process.env.REACT_APP_BACKEND_HOST}/api/register/animal/`, {
+                "method": "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    'Accept': 'application/json',
+                    'auth-token': `${localStorage.getItem('auth_token')}`
+                },
+                body: JSON.stringify({
+                    tagNo: creds.tagNumber,
+                    farmerId: creds.farmerId,
+                    species: creds.species,
+                    breed: creds.breed,
+                    age: creds.age,
+                    noOfCalvings: creds.noOfCalvings
+                }),
+            })
+            if (response.status === 204) {
+                props.setshowAlert("Success", 'Animal data Created Successfully!')
+                setCreds({
+                    tagNumber: "",
+                    date: "",
+                    species: "",
+                    breed: "",
+                    age: "",
+                    noOfCalvings: "",
+                    farmerId: "",
+                    farmerName: ""
+                })
+                return
+            }
+            const data = await response.json();
+            if (Array.isArray(data.errors)) {
+                props.setshowAlert("Error", data.errors[0])
+                return
+            }
+            if (data.error) {
+                props.setshowAlert("Error", data.error)
+                return
+            }
+        }
+        catch (error) {
+            console.log(error);
+            props.setshowAlert("Error", "Internal Server Error")
+        }
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await fetchAnimalSpecies();
+        }
+        fetchData();
+
+    }, []);
+
     return <div>
         <Navbar />
         <div className='container mt-5'>
             <h2 className=''> Register Animal</h2>
-            <form className='mt-3'>
+            <form onSubmit={handleAnimalRegister} className='mt-3'>
+                <div className="input-group mb-3">
+                    <span className="input-group-text" id="famerName">Farmer name:</span>
+                    <input type="text" className="form-control" required={true} placeholder="Farmer Name" aria-label="Farmer name" aria-describedby="farmerName" name="farmerName" value={creds.farmerName} onChange={onChange} />
+                    <button type="button" className='btn btn-primary' onClick={searchFarmer}>Search Farmer</button>
+                </div>
+                {(showFarmer) ?
+                    <select className="form-select form-select mb-3" required={true} name="farmerId" value={creds.farmerId} onChange={onChange} aria-label=".form-select example">
+                        <option>Select a Farmer</option>
+                        {
+                            farmers.map(function (farmer) {
+                                return <option key={farmer.mobileNo} value={`${farmer.mobileNo}`}>{farmer.name} - {farmer.mobileNo}</option>
+                            })
+                        }
+                    </select> : ''}
                 <div className="input-group mb-3">
                     <span className="input-group-text" id="tag-number">Tag number:</span>
-                    <input type="text" className="form-control" required={true} placeholder="Tag Number" aria-label="Full name" aria-describedby="tag-number" />
+                    <input type="text" className="form-control" required={true} placeholder="Tag Number" aria-label="Full name" aria-describedby="tag-number" name="tagNumber" value={creds.tagNumber} onChange={onChange} minLength={3} />
                 </div>
                 <div className="input-group mb-3">
                     <span className="input-group-text" id="date">Date:</span>
-                    <input type="date" className="form-control" required={true} placeholder="Tag Number" aria-label="Date" aria-describedby="date" />
+                    <input type="date" className="form-control" required={true} placeholder="Tag Number" aria-label="Date" aria-describedby="date" name="date" value={creds.date} onChange={onChange} />
                 </div>
-                <select className="form-select form-select mb-3" required={true} aria-label=".form-select-lg example">
-                    <option selected>Species</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+                <select className="form-select form-select mb-3" required={true} aria-label=".form-select-lg example" name="species" value={creds.species} onChange={onChange}>
+                    <option value="">Species</option>
+                    {
+                        (!SpeciesLoading) ?
+                            Object.keys(Species.species).map(species => {
+                                return <option value={species} key={species} >
+                                    {species}
+                                </option>
+                            })
+                            :
+                            ''
+                    }
                 </select>
-                <select className="form-select form-select mb-3" required={true} aria-label=".form-select-sm example">
-                    <option selected>Breed</option>
-                    <option value="1">One</option>
-                    <option value="2">two</option>
-                    <option value="3">three</option>
-                </select>
+                {(creds.species !== "") ?
+                    <select className="form-select form-select mb-3" required={true} aria-label=".form-select-sm example" name="breed" value={creds.breed} onChange={onChange}>
+                        <option value="">Breed</option>
+                        {
+                            (!SpeciesLoading) ?
+                                Species.species[creds.species].map(breed => {
+                                    return <option value={breed} key={breed} >
+                                        {breed}
+                                    </option>
+                                })
+                                :
+                                ''
+                        }
+                    </select>
+                    : ''}
                 <div className="input-group mb-3">
                     <span className="input-group-text" id="age">Age:</span>
-                    <input type="number" minLength={10} required={true} className="form-control" placeholder="Age" aria-label="age" aria-describedby="age" />
+                    <input type="number" minLength={10} required={true} className="form-control" placeholder="Age" aria-label="age" aria-describedby="age" name="age" value={creds.age} onChange={onChange} />
                 </div>
                 <div className="input-group mb-3">
                     <span className="input-group-text" id="calvings">No of calvings:</span>
-                    <input type="number" className="form-control" placeholder="Number of calvings" aria-label="calvings" aria-describedby="calvings" />
+                    <input type="number" className="form-control" placeholder="Number of calvings" aria-label="calvings" aria-describedby="calvings" required={true} name="noOfCalvings" value={creds.noOfCalvings} onChange={onChange} />
                 </div>
                 <button type="submit" className="btn btn-primary mt-5">Register</button>
             </form>
