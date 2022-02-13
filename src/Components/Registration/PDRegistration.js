98e482c698a5c94ab4f436ec4d54ac91a741a0d8
Robin8766 <admin@prodev.pro>
2022-02-13 20:46:59 +0530

    PD registration completed

--- src/Components/Registration/PDRegistration.js
+++ src/Components/Registration/PDRegistration.js
@@ -15,11 +15,18 @@ const PDRegistration = (props) => {
         freshReports: "",
         pdDate: "",
         pdResult: "",
-        pdDays: "",
+        pregnancyDays: "",
         doctorName: ""
     });
+
     const onChange = (e) => {
-        setCreds({ ...creds, [e.target.name]: e.target.value })
+        if (e.target.name === "pdDate" && creds.aiDate !== "") {
+            let aiDate = new Date(creds.aiDate);
+            let pdDate = new Date(e.target.value);
+            setCreds({ ...creds, pregnancyDays: (pdDate.getTime() - aiDate.getTime()) / (1000 * 3600 * 24), [e.target.name]: e.target.value });
+        } else {
+            setCreds({ ...creds, [e.target.name]: e.target.value });
+        }
     }
 
     const fetchAiDetails = async (tagNo) => {
@@ -135,20 +142,71 @@ const PDRegistration = (props) => {
                 },
             })
             const data = await response.json()
-            if (data.success) {
+            if (data.success && data.animals) {
                 let farmerDetails = await fetchFarmer(data.animals.farmerId);
                 let techncianUser = await fetchTechnician(localStorage.getItem('auth_token'), localStorage.getItem('user_type'))
                 let doctorDetails = await fetchDoctor(techncianUser.doctor);
                 setCreds({
-                    ...creds, breed: data.animals.breed, species: data.animals.species, freshReports: aidetails.freshReports, bullId: aidetails.bullId, aiDate: aidetails.date.substring(0, 10),
+                    ...creds,
+                    animalTagNo: tagno,
+                    breed: data.animals.breed, species: data.animals.species, freshReports: aidetails.freshReports, bullId: aidetails.bullId, aiDate: aidetails.date.substring(0, 10),
                     villageName: farmerDetails.village, ownerName: farmerDetails.name, doctorName: doctorDetails.name
                 });
                 if (!data.animals) {
                     props.setshowAlert("Error", "No bull found with the specific tag Number")
                 }
             }
+            else if (!data.animals) {
+                props.setshowAlert("Error", `No details found with the corresponding tag number!`)
+            }
             else {
                 props.setshowAlert("Error", `${data.error}`)
+                return
+            }
+        }
+        catch (error) {
+            console.log(error);
+            props.setshowAlert("Error", "Internal Server Error")
+        }
+    }
+
+    const registerPD = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(`${process.env.REACT_APP_BACKEND_HOST}/api/register/pd/`, {
+                "method": "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    'Accept': 'application/json',
+                    'auth-token': `${localStorage.getItem('auth_token')}`
+                },
+                body: JSON.stringify({ ...creds }),
+            })
+            if (response.status === 204) {
+                props.setshowAlert("Success", 'Ai details created!')
+                setCreds({
+                    animalTagNo: "",
+                    bullId: "",
+                    villageName: "",
+                    ownerName: "",
+                    aiDate: "",
+                    breed: "",
+                    species: "",
+                    freshReports: "",
+                    pdDate: "",
+                    pdResult: "",
+                    pregnancyDays: "",
+                    doctorName: ""
+                })
+                return
+            }
+            const data = await response.json();
+            if (Array.isArray(data.errors)) {
+                props.setshowAlert("Error", data.errors[0])
+                return
+            }
+            if (data.error) {
+                props.setshowAlert("Error", data.error)
                 return
             }
         }
@@ -163,7 +221,7 @@ const PDRegistration = (props) => {
         <Navbar />
         <div className='container mt-5'>
             <h2 className=''> Register PD Details</h2>
-            <form className='mt-3'>
+            <form onSubmit={registerPD} className='mt-3'>
                 <div className="input-group mb-3">
                     <span className="input-group-text" id="tag-number">Tag number:</span>
                     <input type="number" className="form-control" required={true} placeholder="Enter bull Tag Number" aria-label="Tag Number" aria-describedby="tag-number" value={tagNo} onChange={(e) => settagNo(e.target.value)} />
@@ -204,13 +262,13 @@ const PDRegistration = (props) => {
                     <input type="date" className="form-control" required={true} placeholder="PD date" aria-label="Date" aria-describedby="date" onChange={onChange} value={creds.pdDate} name="pdDate" />
                 </div>
                 <select className="form-select form-select mb-3" required={true} aria-label=".form-select-lg example" value={creds.pdResult} name="pdResult" onChange={onChange}>
-                    <option>PD Result</option>
+                    <option value="">PD Result</option>
                     <option value="pregnant">Pregnant</option>
                     <option value="non-pregnant">Non Pregnant</option>
                 </select>
                 <div className="input-group mb-3">
                     <span className="input-group-text" id="tag-number">PD days:</span>
-                    <input type="number" className="form-control" required={true} placeholder="Pd days" aria-label="pd days" aria-describedby="pd-days" value={creds.pdDays} name="pdDays" disabled />
+                    <input type="number" className="form-control" required={true} placeholder="Pd days" aria-label="pd days" aria-describedby="pd-days" value={creds.pregnancyDays} name="pregnancyDays" disabled />
                 </div>
                 <div className="input-group mb-3">
                     <span className="input-group-text" id="calvings">VD USER ID:</span>
